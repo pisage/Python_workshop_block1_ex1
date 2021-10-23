0fd4641965350d8fad20b02a40bba1117ad18795
pisage <pisarz.to@gmail.com>
2021-10-23 11:38:35 +0200

    add workshop exercise 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python_workshop_block_1
+Guess the number game. Program chooses the random number in range 1-100 and user has to guess.
+When the entered value is too small or too big, the user gets the corresponding message.
---
+++ ex1_guess_number.py
@@ -0,0 +1,31 @@
+import random
+
+"""
+Guess the number game. Program chooses the random number in range 1-100 and user has to guess.
+When the entered value is too small or too big, the user gets the corresponding message.
+
+After giving a letter instead of number, the warning is showed. 
+:comp_picked_number: int
+:return: number chosen randomly by the computer
+
+:user_picked_numeber: int
+:return: consecutive guesses by the user
+"""
+comp_picked_number = random.randint(1,100)
+while True:
+    try:
+        user_picked_number = int((input("Guess the number: ")))
+        if (user_picked_number > 100) or (user_picked_number < 0):
+            print("Number out of range")
+        else:
+            if user_picked_number < comp_picked_number:
+                print("Too small")
+            elif user_picked_number > comp_picked_number:
+                print("Too big")
+            elif user_picked_number == comp_picked_number:
+                print("You win!")
+                break
+    except ValueError:
+        print("It's not a number!")
+
+
